9bf55ddde7e2dc215e8582e67fbeb0fd311784bb
zwill22 <73105122+zwill22@users.noreply.github.com>
2024-02-12 16:52:37 +0000

    Setup CMakeLists.txt

---
+++ libuw12.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zack Williams on 12/02/2024.
+//
+
+#include "libuw12.hpp"
---
+++ libuw12.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Zack Williams on 12/02/2024.
+//
+
+#ifndef LIBUW12_HPP
+#define LIBUW12_HPP
+
+#endif //LIBUW12_HPP
