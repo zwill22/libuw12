ca1ec68a5a5ceea2dad95c2902e0d3e99019d02a
zwill22 <73105122+zwill22@users.noreply.github.com>
2024-02-23 17:40:38 +0000

    Added integral_functions.hpp and tests

---
+++ src/integrals/integral_functions.hpp
@@ -0,0 +1,55 @@
+//
+// Created by Zack Williams on 27/11/2020.
+//
+
+#ifndef UW12_INTEGRAL_FUNCTIONS_HPP
+#define UW12_INTEGRAL_FUNCTIONS_HPP
+
+#include <functional>
+#include <vector>
+
+#include "../utils/linalg.hpp"
+#include "../utils/parallel.hpp"
+
+namespace uw12::integrals {
+    /// Function which returns a matrix of two-index integrals
+    using TwoIndexFn = std::function<linalg::Mat()>;
+
+    /// Function which returns a matrix of three-index integrals for a single shell
+    /// of third index
+    using ThreeIndexFn = std::function<linalg::Mat(int)>;
+
+    /// \brief Calculate the three-index Coulomb matrix \f$(\mu\nu | w | A)\f$
+    ///
+    /// Use a `three_index_fn` for each density-fitting basis shell to calculate
+    /// the total three-index matrix
+    ///
+    /// \param three_index_fn Function to calculate integrals for each basis shell
+    /// \param df_offsets offsets in full matrix for each df basis shell
+    /// \param n_rows number of rows of the matrix (depends on the `three_index_fn`)
+    /// \param n_df number of df basis functions
+    ///
+    /// \return Three-index matrix
+    inline linalg::Mat coulomb_3idx(const ThreeIndexFn &three_index_fn,
+                                    const std::vector<int> &df_offsets, const int n_rows,
+                                    const int n_df) {
+        linalg::Mat result(n_rows, n_df);
+
+        const auto n_df_sh = df_offsets.size();
+
+        const auto parallel_fn = [&result, &three_index_fn,
+                    &df_offsets](const int A) {
+            const auto off_a = df_offsets[A];
+
+            const auto shell_results = three_index_fn(A);
+
+            linalg::assign_cols(result, shell_results, off_a);
+        };
+
+        parallel::parallel_for(0, n_df_sh, parallel_fn);
+
+        return result;
+    }
+} // namespace uw12::integrals
+
+#endif  // UW12_INTEGRAL_FUNCTIONS_HPP
---
+++ tests/test_integral_functions.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Zack Williams on 23/02/2024.
+//
+
+#include "../src/integrals/integral_functions.hpp"
+
+#include "catch.hpp"
+
+TEST_CASE("Test integrals - Test integral functions") {
+    const std::vector df_shell_sizes = {1, 3, 5, 7, 9, 11};
+    std::vector df_offsets = {0};
+    for (int idx = 0; idx < df_shell_sizes.size() - 1; ++idx) {
+        df_offsets.push_back(df_offsets[idx] + df_shell_sizes[idx]);
+    }
+
+    constexpr auto n_ao = 5;
+    constexpr auto n_row = n_ao * (n_ao + 1) / 2;
+
+    auto n_df = 0;
+    for (const auto df: df_shell_sizes) {
+        n_df += df;
+    }
+
+    const auto three_idx_fn = [n_row, &df_shell_sizes](const int A) -> uw12::linalg::Mat {
+        return uw12::linalg::ones(n_row, df_shell_sizes[A]) * A;
+    };
+
+    REQUIRE_THROWS(uw12::integrals::coulomb_3idx(three_idx_fn, df_offsets, n_row + 1, n_df));
+    REQUIRE_THROWS(uw12::integrals::coulomb_3idx(three_idx_fn, df_offsets, n_row - 1, n_df));
+    REQUIRE_THROWS(uw12::integrals::coulomb_3idx(three_idx_fn, df_offsets, n_row, n_df - 1));
+
+    const auto result = uw12::integrals::coulomb_3idx(three_idx_fn, df_offsets, n_row, n_df);
+    REQUIRE(uw12::linalg::n_rows(result) == n_row);
+    REQUIRE(uw12::linalg::n_cols(result) == n_df);
+
+    int shell_idx = 0;
+    for (int col_idx = 0; col_idx < n_df; ++col_idx) {
+        for (int idx = 0; idx < df_offsets.size(); ++idx) {
+            if (col_idx >= df_offsets[idx]) {
+                shell_idx = idx;
+            }
+        }
+
+        const uw12::linalg::Mat col = uw12::linalg::col(result, col_idx);
+        const uw12::linalg::Mat target = uw12::linalg::ones(n_row, 1) * shell_idx;
+
+        CHECK(uw12::linalg::nearly_equal(col, target, 1e-10));
+    }
+}
