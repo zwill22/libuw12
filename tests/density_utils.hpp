802eb386fc2406622e16ed9851a153e1d7c579d6
zwill22 <73105122+zwill22@users.noreply.github.com>
2024-03-07 11:33:25 +0000

    Restored density_utils.hpp and tests

---
+++ tests/density_utils.hpp
@@ -0,0 +1,97 @@
+//
+// Created by Zack Williams on 01/03/2024.
+//
+
+#ifndef DENSITY_UTILS_HPP
+#define DENSITY_UTILS_HPP
+
+#include <iostream>
+
+#include "../src/utils/utils.hpp"
+
+namespace density {
+
+/// Calculates a random density matrix with number of spin channels determined
+/// by length of `n_occ` and size `n_ao * n_ao`.
+///
+/// @param n_occ Occupied orbitals for each spin channel
+/// @param n_ao
+/// @param seed
+///
+/// @return
+inline uw12::utils::DensityMatrix random_density_matrix(
+    const std::vector<size_t> &n_occ, const size_t n_ao, const int seed
+) {
+  using uw12::linalg::random;
+
+  uw12::utils::Orbitals C;
+  uw12::utils::Occupations occ;
+
+  const auto n_spin = n_occ.size();
+  const auto max = n_spin == 1 ? 2 : 1;
+
+  for (auto sigma = 0; sigma < n_spin; sigma++) {
+    C.push_back(random(n_ao, n_occ[sigma], seed));
+    const uw12::linalg::Vec tmp = random(n_occ[sigma], 1, seed);
+    occ.emplace_back(max * tmp);
+  }
+
+  const auto Co = uw12::utils::occupation_weighted_orbitals(C, occ);
+
+  return uw12::utils::construct_density(Co);
+}
+
+/// Calculates the orbitals from a single density matrix `D`
+///
+/// @param D Density matrix for a single spin channel
+/// @param epsilon Threshold for zero occupation
+///
+/// @return Orbitals and occupations
+inline std::pair<uw12::linalg::Mat, uw12::linalg::Vec> orbitals_from_density(
+    const uw12::linalg::Mat &D, const double epsilon
+) {
+  const auto n_ao = uw12::linalg::n_rows(D);
+  assert(uw12::linalg::n_cols(D) == n_ao);
+  assert(epsilon > 0);
+
+  if (uw12::linalg::nearly_equal(
+          D, uw12::linalg::zeros(n_ao, n_ao), test::epsilon
+      )) {
+    return {uw12::linalg::zeros(n_ao, 1), uw12::linalg::ones(1)};
+      }
+
+  const uw12::linalg::Mat D_neg = -0.5 * (D + uw12::linalg::transpose(D));
+
+  const auto [occ_neg, C] =
+      uw12::linalg::eigen_decomposition(D_neg, epsilon, epsilon);
+
+  const uw12::linalg::Vec occ = -1 * occ_neg;
+
+  return {C, occ};
+}
+
+/// Calculates the occupation weighted orbitals from a density matrix
+///
+/// @param D Density matrix
+/// @param epsilon Threshold for zero occupation
+///
+/// @return Occupation weighted orbitals
+inline uw12::utils::Orbitals calculate_orbitals_from_density(
+    const uw12::utils::DensityMatrix &D, const double epsilon
+) {
+  uw12::utils::Orbitals orbitals;
+  uw12::utils::Occupations occupations;
+
+  const auto n_spin = D.size();
+
+  for (size_t sigma = 0; sigma < n_spin; ++sigma) {
+    const auto [C, occ] = orbitals_from_density(D[sigma], epsilon);
+    orbitals.push_back(C);
+    occupations.push_back(occ);
+  }
+
+  return uw12::utils::occupation_weighted_orbitals(orbitals, occupations);
+}
+}  // namespace density
+
+#endif
---
+++ tests/test_density_utils.cpp
@@ -0,0 +1,82 @@
+//
+// Created by Zack Williams on 04/03/2024.
+//
+
+#include "../src/utils/utils.hpp"
+#include "catch.hpp"
+#include "density_utils.hpp"
+
+using density::calculate_orbitals_from_density;
+using density::orbitals_from_density;
+using density::random_density_matrix;
+using test::epsilon;
+using test::seed;
+
+using uw12::linalg::n_cols;
+using uw12::linalg::n_elem;
+using uw12::linalg::n_rows;
+using uw12::linalg::nearly_equal;
+
+TEST_CASE("Orbitals from density") {
+  constexpr size_t n_ao = 10;
+  std::vector<size_t> n_occ = {4};
+
+  for (size_t i = 0; i < 2; ++i) {
+    const auto n_spin = n_occ.size();
+    const auto D = random_density_matrix(n_occ, n_ao, seed);
+    REQUIRE((D.size() == n_spin));
+
+    for (const auto &Do : D) {
+      REQUIRE((n_rows(Do) == n_ao));
+      REQUIRE((n_cols(Do) == n_ao));
+    }
+
+    SECTION("Orbitals from density") {
+      std::vector<uw12::linalg::Mat> orbs = {};
+      std::vector<uw12::linalg::Vec> occs = {};
+      for (size_t sigma = 0; sigma < n_spin; ++sigma) {
+        const auto [C, occ] = orbitals_from_density(D[sigma], 1e-6);
+        REQUIRE((n_rows(C) == n_ao));
+        REQUIRE((n_cols(C) == n_occ[sigma]));
+        REQUIRE((n_elem(occ) == n_occ[sigma]));
+        orbs.push_back(C);
+        occs.push_back(occ);
+      }
+
+      const auto orbitals = calculate_orbitals_from_density(D, 1e-6);
+      REQUIRE((orbitals.size() == n_spin));
+
+      const auto orbitals2 =
+          uw12::utils::occupation_weighted_orbitals(orbs, occs);
+      REQUIRE((orbitals2.size() == n_spin));
+      for (size_t sigma = 0; sigma < n_spin; ++sigma) {
+        CHECK(nearly_equal(orbitals[sigma], orbitals2[sigma], epsilon));
+      }
+    }
+
+    SECTION("Test invertability") {
+      const auto orbitals = calculate_orbitals_from_density(D, 1e-6);
+      REQUIRE((orbitals.size() == n_spin));
+
+      const auto D2 = uw12::utils::construct_density(orbitals);
+      REQUIRE((D2.size() == n_spin));
+      for (size_t sigma = 0; sigma < n_spin; ++sigma) {
+        CHECK(nearly_equal(D[sigma], D2[sigma], epsilon));
+      }
+    }
+
+    n_occ.push_back(3);
+  }
+
+  SECTION("Zero density") {
+    const auto D = uw12::linalg::zeros(n_ao, n_ao);
+
+    const auto [C, occ] = orbitals_from_density(D, 1e-6);
+    REQUIRE((n_rows(C) == n_ao));
+    REQUIRE((n_cols(C) == 1));
+    REQUIRE((n_elem(occ) == 1));
+
+    CHECK(nearly_equal(C, uw12::linalg::zeros(n_ao, 1), epsilon));
+    CHECK(nearly_equal(occ, uw12::linalg::ones(1), epsilon));
+  }
+}
