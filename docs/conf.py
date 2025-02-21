2fe373f582ca50d88002c51123c6fbb0c35ddc1a
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-02-21 22:10:48 +0000

    New documentation

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+import textwrap
+
+project = 'libuw12'
+copyright = '2024, Z. M. Williams'
+author = 'Z. M. Williams'
+release = '0.1.0'
+
+extensions = ['sphinx.ext.mathjax', 'breathe', 'exhale', 'sphinx_rtd_theme']
+
+breathe_projects = {"libuw12": "./_doxygen/xml/"}
+breathe_default_project = "libuw12"
+
+# Setup the exhale extension
+exhale_args = {
+    # These arguments are required
+    "containmentFolder":     "./api",
+    "rootFileName":          "library_root.rst",
+    "doxygenStripFromPath":  "..",
+    "rootFileTitle":         "libuw12 API",
+    # Suggested optional arguments
+    "createTreeView":        True,
+    "exhaleExecutesDoxygen": True,
+    "exhaleDoxygenStdin":   textwrap.dedent('''
+    INPUT = ../
+    TOC_INCLUDE_HEADINGS = 2
+    EXTRACT_ALL = NO
+    EXTRACT_LOCAL_CLASSES = NO
+    EXCLUDE_PATTERNS = "*test*" "example" "cmake" "docs"
+    PROJECT_NAME = "libuw12"
+    ''')
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Tell sphinx what the primary language being documented is.
+primary_domain = 'cpp'
+
+# Tell sphinx what the pygments highlight language should be.
+highlight_language = 'cpp'
+todo_include_todos = False
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
